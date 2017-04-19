2625276afd2d3702bd50f1fc1d739a65326a43bf
LeChristmas <1005943@student.sae.edu.au>
2017-04-19 14:24:30 +1000

    Week 11 Update 6

---
+++ The_Last_Guest_Unity_Project/The_Last_Guest_Unity_Project/Assets/Scripts/Player/Stun_Pick_Up.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stun_Pick_Up : MonoBehaviour
+{
+    public float pick_up_distance = 2.0f;
+
+    private Gameobject player;
+    private Transform player_transform;
+
+    private Gameobject items;
+    private Combined_Items activator;
+
+    void Start ()
+    {
+        player = GameObject.Find("Player");
+        player_transform = player.GetComponent<Transform>();
+
+        items = Gameobject.Find("Items");
+        activator = items.GetComponent<Combined_Items>();
+    }
+
+    void Update ()
+    {
+        vector3 trarget_distance = player_transform.postion - player_transform.position;
+        float distance = Vector3.distance(target_distance, foransform.forward);
+    }
+
+        if (distance<dis)
+        {
+            activator.Stun_Activate();
+}
+}
