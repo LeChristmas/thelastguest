36de61d64f2cfc1d2565fdad94e89e44bdbdf1a1
Christopher Wrigley <1005943@student.sae.edu.au>
2017-04-20 18:45:37 +1000

    Week 11 Update 7

---
+++ The_Last_Guest_Unity_Project/The_Last_Guest_Unity_Project/Assets/Scripts/Player/Flash_Pick_Up.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flash_Pick_Up : MonoBehaviour
+{
+    public float pick_up_distance = 2.5f;
+
+    private GameObject player;
+    private Transform player_transform;
+
+    private GameObject items;
+    private Combined_Items activator;
+
+    void Start()
+    {
+        player = GameObject.Find("Player_Camera");
+        player_transform = player.GetComponent<Transform>();
+
+        items = GameObject.Find("Items");
+        activator = items.GetComponent<Combined_Items>();
+    }
+
+    void Update()
+    {
+        Vector3 target_distance = player_transform.position - transform.position;
+        float distance = Vector3.Distance(target_distance, transform.forward);
+
+        Debug.Log("distance is " + distance);
+
+        if (distance < pick_up_distance)
+        {
+            activator.Flash_Activate();
+            Destroy(gameObject);
+        }
+    }
+}
--- The_Last_Guest_Unity_Project/The_Last_Guest_Unity_Project/Assets/Scripts/Player/Stun_Pick_Up.cs
+++ The_Last_Guest_Unity_Project/The_Last_Guest_Unity_Project/Assets/Scripts/Player/Stun_Pick_Up.cs
@@ -1,35 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Stun_Pick_Up : MonoBehaviour
 {
-    public float pick_up_distance = 2.0f;
+    public float pick_up_distance = 2.5f;
 
-    private Gameobject player;
+    private GameObject player;
     private Transform player_transform;
 
-    private Gameobject items;
+    private GameObject items;
     private Combined_Items activator;
 
-    void Start ()
+    void Start()
     {
         player = GameObject.Find("Player");
         player_transform = player.GetComponent<Transform>();
 
-        items = Gameobject.Find("Items");
+        items = GameObject.Find("Items");
         activator = items.GetComponent<Combined_Items>();
     }
 
-    void Update ()
+    void Update()
     {
-        vector3 trarget_distance = player_transform.postion - player_transform.position;
-        float distance = Vector3.distance(target_distance, foransform.forward);
-    }
+        Vector3 target_distance = player_transform.position - transform.position;
+        float distance = Vector3.Distance(target_distance, transform.forward);
 
-        if (distance<dis)
+        if (distance < pick_up_distance)
         {
             activator.Stun_Activate();
-}
+            Destroy(gameObject);
+        }
+    }
 }
